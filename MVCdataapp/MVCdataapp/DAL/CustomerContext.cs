7b4226be3754109ce8ca9795726bb89d800aeed5
jiljo <jiljokgeorge@mca.ajce.in>
2019-03-02 05:27:38 +0000

    mvc database

---
+++ MVCdataapp/MVCdataapp/DAL/CustomerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVCdataapp.Models;
+
+namespace MVCdataapp.DAL
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(): base("mycon")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCdataapp.Models.Customer> Customers { get; set; }
+    }
+}
---
+++ MVCdataapp/MVCdataapp/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCdataapp.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [DataType(DataType.MultilineText)]
+        public string name { get; set; }
+
+
+        
+        [Required(ErrorMessage = "Can't be empty")]
+        [DataType(DataType.MultilineText)]
+        public string address { get; set; }
+
+
+       
+        [Required(ErrorMessage = "Enter a valid email")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        
+        [Required(ErrorMessage = "Enter a valid name")]
+        [DataType(DataType.Text)]
+        public string username { get; set; }
+
+       
+        [Required(ErrorMessage = "Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+    }
+}
